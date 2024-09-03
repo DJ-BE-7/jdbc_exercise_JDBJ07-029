09e206812625f59b4793ac9718d2a06e90a81e52
choeunlee <choeun1500@gmail.com>
2024-09-03 15:53:21 +0900

    [실습1] STUDENT CLUB FINISHED

--- src/main/java/com/nhnacademy/jdbc/club/repository/impl/ClubRegistrationRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/club/repository/impl/ClubRegistrationRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.jdbc.club.repository.impl;
 
+import com.nhnacademy.jdbc.club.domain.Club;
 import com.nhnacademy.jdbc.club.domain.ClubStudent;
 import com.nhnacademy.jdbc.club.repository.ClubRegistrationRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -16,62 +17,260 @@ public class ClubRegistrationRepositoryImpl implements ClubRegistrationRepositor
     @Override
     public int save(Connection connection, String studentId, String clubId) {
         //todo#11 - 핵생 -> 클럽 등록, executeUpdate() 결과를 반환
-
-        return 0;
+        int result = 0;
+        try {
+            String sql = "insert into jdbc_club_registrations (student_id, club_id) values (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, studentId);
+            preparedStatement.setString(2, clubId);
+            result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public int deleteByStudentIdAndClubId(Connection connection, String studentId, String clubId) {
         //todo#12 - 핵생 -> 클럽 탈퇴, executeUpdate() 결과를 반환
-        return 0;
+        int result = 0;
+        try {
+            String sql = "delete from jdbc_club_registrations where student_id = ? and club_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, studentId);
+            preparedStatement.setString(2, clubId);
+            result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public List<ClubStudent> findClubStudentsByStudentId(Connection connection, String studentId) {
         //todo#13 - 핵생 -> 클럽 등록, executeUpdate() 결과를 반환
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new ArrayList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s inner join jdbc_club_registrations r on s.id = r.student_id " +
+                    "inner join jdbc_club c on r.club_id = c.club_id " +
+                    "where s.id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, studentId);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
 
     @Override
     public List<ClubStudent> findClubStudents(Connection connection) {
         //todo#21 - join
-        return Collections.emptyList();
-    }
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new LinkedList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s inner join jdbc_club_registrations r on s.id = r.student_id " +
+                    "inner join jdbc_club c on r.club_id = c.club_id";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+    }return clubStudents;}
 
     @Override
     public List<ClubStudent> findClubStudents_left_join(Connection connection) {
         //todo#22 - left join
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new ArrayList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s left join jdbc_club_registrations r on s.id = r.student_id " +
+                    "left join jdbc_club c on r.club_id = c.club_id";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
 
     @Override
     public List<ClubStudent> findClubStudents_right_join(Connection connection) {
         //todo#23 - right join
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new ArrayList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s inner join jdbc_club_registrations r on s.id = r.student_id " +
+                    "right join jdbc_club c on r.club_id = c.club_id";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
 
     @Override
     public List<ClubStudent> findClubStudents_full_join(Connection connection) {
         //todo#24 - full join = left join union right join
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new ArrayList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s left join jdbc_club_registrations r on s.id = r.student_id " +
+                    "left join jdbc_club c on r.club_id = c.club_id union " +
+                    "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s inner join jdbc_club_registrations r on s.id = r.student_id " +
+                    "right join jdbc_club c on r.club_id = c.club_id";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
 
     @Override
     public List<ClubStudent> findClubStudents_left_excluding_join(Connection connection) {
         //todo#25 - left excluding join
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new ArrayList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s left join jdbc_club_registrations r on s.id = r.student_id " +
+                    "left join jdbc_club c on r.club_id = c.club_id " +
+                    "where c.club_id is null";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
 
     @Override
     public List<ClubStudent> findClubStudents_right_excluding_join(Connection connection) {
         //todo#26 - right excluding join
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new ArrayList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s inner join jdbc_club_registrations r on s.id = r.student_id " +
+                    "right join jdbc_club c on r.club_id = c.club_id " +
+                    "where s.id is null;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
 
     @Override
     public List<ClubStudent> findClubStudents_outher_excluding_join(Connection connection) {
         //todo#27 - outher_excluding_join = left excluding join union right excluding join
-        return Collections.emptyList();
+        ResultSet resultSet = null;
+        List<ClubStudent> clubStudents = new LinkedList<>();
+        ClubStudent clubStudent = null;
+        try {
+            String sql = "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s left join jdbc_club_registrations r on s.id = r.student_id " +
+                    "left join jdbc_club c on r.club_id = c.club_id " +
+                    "where c.club_id is null union " +
+                    "select s.id, s.name, c.club_id, c.club_name " +
+                    "from jdbc_students s inner join jdbc_club_registrations r on s.id = r.student_id " +
+                    "right join jdbc_club c on r.club_id = c.club_id " +
+                    "where s.id is null";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                clubStudent = new ClubStudent(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                clubStudents.add(clubStudent);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clubStudents;
     }
-
 }
--- src/main/java/com/nhnacademy/jdbc/club/repository/impl/ClubRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/club/repository/impl/ClubRepositoryImpl.java
@@ -14,34 +14,94 @@ public class ClubRepositoryImpl implements ClubRepository {
     @Override
     public Optional<Club> findByClubId(Connection connection, String clubId) {
         //todo#3 club 조회
-
-        return Optional.empty();
+        ResultSet resultSet = null;
+        Club club = null;
+        try {
+            String sql = "select * from jdbc_club where club_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, clubId);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                club = new Club(resultSet.getString("club_id"),
+                        resultSet.getString("club_name"),
+                        resultSet.getTimestamp("club_created_at").toLocalDateTime());
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.ofNullable(club);
     }
 
     @Override
     public int save(Connection connection, Club club) {
         //todo#4 club 생성, executeUpdate() 결과를 반환
-        return 0;
+        int result = 0;
+        try {
+            String sql = "insert into jdbc_club (club_id, club_name, club_created_at) values (?, ?, DEFAULT)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, club.getClubId());
+            preparedStatement.setString(2, club.getClubName());
+            result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public int update(Connection connection, Club club) {
         //todo#5 club 수정, clubName을 수정합니다. executeUpdate()결과를 반환
-
-        return 0;
+        int result = 0;
+        try {
+            String sql = "update jdbc_club set club_name = ? where club_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, club.getClubName());
+            preparedStatement.setString(2, club.getClubId());
+            result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public int deleteByClubId(Connection connection, String clubId) {
         //todo#6 club 삭제, executeUpdate()결과 반환
-
-        return 0;
+        int result = 0;
+        try {
+            String sql = "delete from jdbc_club where club_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, clubId);
+            result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public int countByClubId(Connection connection, String clubId) {
         //todo#7 clubId에 해당하는 club의 count를 반환
-
-        return 0;
+        ResultSet resultSet = null;
+        int count = 0;
+        try {
+            String sql = "select count(*) from jdbc_club where club_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, clubId);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return count;
     }
 }
